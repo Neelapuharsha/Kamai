f86b0501e4a0d7222beb3ae581f80ae0bbe3dace
neelapuharsha <neelapuharsha@gmail.com>
2024-06-12 15:51:11 +0530

    parallax effect with header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import ParallaxComponent from "./components/Parallax/ParallaxComponent"
 import './App.css';
-
-
+import Section from './components/Section/Section';
+import Header from './components/Header/Header'
 
 function App() {
   return (
+      
     <div className="App">
-      <ParallaxComponent/>
+    <Header />
+      <Section 
+        backgroundColor="teal" 
+        borderTopRightRadius="0px" 
+        borderTopLeftRadius="0px" 
+      />
+      <Section 
+        backgroundColor="coral" 
+        borderTopRightRadius="40px" 
+        borderTopLeftRadius="40px" 
+      />
+      <Section 
+        backgroundColor="#E6E6FA" 
+        borderTopRightRadius="40px" 
+        borderTopLeftRadius="40px" 
+      />
     </div>
   );
 }
