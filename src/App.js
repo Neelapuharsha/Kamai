5ccc234626853f0bdc4823b2f7629bda71d1b86b
neelapuharsha <neelapuharsha@gmail.com>
2024-06-16 04:04:37 +0530

    cursor added 4:04am

--- src/App.js
+++ src/App.js
@@ -7,33 +7,38 @@ import Footer from './components/Footer/Footer';
 import HeroSection from './components/Landing page/HeroSection';
 import Button from './components/Buttons/Button';
 import FlamingoComponent from './components/moreFeatures/FlamingoComponent';
+import MagicCursor from './components/MagicCursor/MagicCursor';
 
 function App() {
-  
+
   return (
 
     <div className="App">
-    <Header />
-      <Section 
-        backgroundColor="teal" 
-        borderTopRightRadius="0px" 
-        borderTopLeftRadius="0px" 
+      <MagicCursor />
+      <Header />
+      <Section
+        backgroundColor="teal"
+        borderTopRightRadius="0px"
+        borderTopLeftRadius="0px"
       >
-    <HeroSection/>
+        <HeroSection />
       </Section>
-      <Section 
-        backgroundColor="coral" 
-        borderTopRightRadius="40px" 
-        borderTopLeftRadius="40px" 
-      > 
- <FlamingoComponent />
+      <Section
+        backgroundColor="coral"
+        borderTopRightRadius="40px"
+        borderTopLeftRadius="40px"
+      >
+        <FlamingoComponent />
 
-            </Section>
-      <Section 
-        backgroundColor="#E6E6FA" 
-        borderTopRightRadius="40px" 
-        borderTopLeftRadius="40px" 
-      />
+      </Section>
+      <Section
+        backgroundColor="#111"
+        borderTopRightRadius="40px"
+        borderTopLeftRadius="40px"
+      >
+        
+      </Section>
+      
     </div>
   );
 }
