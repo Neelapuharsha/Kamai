f113807d89466fc2eceb8966322d5089b679ac63
KiranS2002 <madhusak2002@gmail.com>
2024-06-15 18:05:07 +0530

    buttons changes

--- src/components/Landing page/ImageComponent.js
+++ src/components/Landing page/ImageComponent.js
@@ -1,28 +1,24 @@
-
-
-// import React from 'react';
-// import './ImageStyles.css'; // Import your separate image styles CSS file
-
-// const ImageComponent = ({ src, alt, className, style, ...restProps }) => {
-//     return <img src={src} alt={alt} className={className} style={style} {...restProps} />;
-// };
-
-// export default ImageComponent;
-
-// ImageComponent.js
-
 import React from 'react';
 import './ImageStyles.css'; // Import your separate image styles CSS file
 
-const ImageComponent = ({ src, alt, className, style, borderRadius, ...restProps }) => {
-    const imageStyle = {
-        ...style,
-        borderRadius: borderRadius || '50%', // Default to 50% if borderRadius prop is not provided
-    };
+const ImageComponent = ({ src, alt, className, style, ...restProps }) => {
+    // Define default props
+    const defaultClassName = "image"; // Default CSS class
+    const defaultStyle = {}; // Default inline styles
 
-    return <img src={src} alt={alt} className={className} style={imageStyle} {...restProps} />;
+    // Combine provided and default props
+    const combinedClassName = className ? `${defaultClassName} ${className}` : defaultClassName;
+    const combinedStyle = { ...defaultStyle, ...style };
+
+    return (
+        <img
+            src={src}
+            alt={alt}
+            className={combinedClassName}
+            style={combinedStyle}
+            {...restProps}
+        />
+    );
 };
 
 export default ImageComponent;
-
-
