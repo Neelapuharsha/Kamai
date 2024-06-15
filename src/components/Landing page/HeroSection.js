d734cb7fc22ec7bb8349ca86d8973ed420677d3f
alishashaik360 <alishashaik360@gmail.com>
2024-06-15 17:08:54 +0530

    modified landingpage  by ali

--- src/components/Landing page/HeroSection.js
+++ src/components/Landing page/HeroSection.js
@@ -1,85 +1,77 @@
-
-// import React from 'react';
-// import './HeroSection.css';
-// import ImageComponent from './ImageComponent'; // Ensure correct path
-// import logo from './images/image.png'; 
-// import student1 from './images/s1.png'; 
-// import student2 from './images/f2.png'; 
-// import student3 from './images/f1.png'; 
-// import student4 from './images/s4.png'; 
-
-// const HeroSection = () => {
-//     return (
-//       <section className="hero">
-//         <div className="hero-content">
-//           <div className="hero-text">
-//             <div className="hero-icon">
-//               <ImageComponent src={logo} alt="Icon" className="icon-img" />
-//             </div>
-//             <h1>Build Your Future, Grab your Dream Job</h1>
-//             <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. <strong>Velitofficia consequat.</strong></p>
-//             <button className="hero-button">Get Started</button>
-//           </div>
-          
-//           <div className="hero-images">
-//           {/* Example of using ImageComponent with different borderRadius */}
-//           <ImageComponent src={student1} alt="Student 1" className="hero-image" borderRadius="50%" />
-//           <ImageComponent src={student2} alt="Student 2" className="hero-image" borderRadius="59px 0px 59px 0px" />
-//           <ImageComponent src={student3} alt="Student 3" className="hero-image" borderRadius="59px 0px 59px 0px" />
-//           <ImageComponent src={student4} alt="Student 4" className="hero-image" borderRadius="50%" />
-//         </div>
-//         </div>
-//       </section>
-//     );
-// };
-
-// export default HeroSection;
-
 import React, { useState, useEffect } from 'react';
 import './HeroSection.css';
-import ImageComponent from './ImageComponent'; // Ensure correct path
-import logo from './images/image.png'; 
-import student1 from './images/s1.png'; 
-import student2 from './images/f2.png'; 
-import student3 from './images/f1.png'; 
-import student4 from './images/s4.png'; 
+import ImageComponent from './ImageComponent';
+import logo from './images/image.png';
+import student1 from './images/s1.png';
+import student2 from './images/f2.png';
+import student3 from './images/f1.png';
+import student4 from './images/s4.png';
+import student5 from './images/f3.png';
+import student6 from './images/f4.png';
+import student7 from './images/f5.png';
 import Button from '../Buttons/Button';
+import TextAnimation from './TextAnimation';
 
 const HeroSection = () => {
-    const images = [ student1, student2, student3, student4];
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [student1, student2, student3, student4];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [fadeIn, setFadeIn] = useState(true);
 
-    useEffect(() => {
-        // Function to rotate images every 4 seconds
-        const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 5000);
+  useEffect(() => {
+    
+    const interval = setInterval(() => {
+      setFadeIn(false); 
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setFadeIn(true);
+      }, 900); 
+    },6000);
 
-        return () => clearInterval(interval); // Cleanup the interval on component unmount
-    }, [images.length]); // Dependency array ensures useEffect runs only on mount and when images.length changes
+    return () => clearInterval(interval); 
+  }, [images.length]);
 
-    return (
-      <section className="hero">
-        <div className="hero-content">
-          <div className="hero-text">
-            <div className="hero-icon">
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <div className="hero-text">
+          <div className="hero-icon">
             
-            </div>
-            <h1>Build Your Future, Grab your Dream Job</h1>
-            <p>Transform your career aspirations into achievements and embrace a future filled with passion and purpose with <strong> KamAi</strong> by your side! </p>
-            <Button className="primary-btn" text="Download Our App" />
           </div>
-          
-          <div className="hero-images">
-            {/* Example of using ImageComponent with different borderRadius */}
-            <ImageComponent src={images[(currentIndex + 1) % images.length]} alt="Student 1" className="hero-image" borderRadius="50%" />
-            <ImageComponent src={images[(currentIndex + 2) % images.length]} alt="Student 2" className="hero-image" borderRadius="94px 0px " />
-            <ImageComponent src={images[(currentIndex + 3) % images.length]} alt="Student 3" className="hero-image" borderRadius="94px 0px " />
-            <ImageComponent src={images[(currentIndex + 4) % images.length]} alt="Student 4" className="hero-image" borderRadius="50%" />
-          </div>
+          <h1>Build Your Future, Grab your Dream Job</h1>
+          <p>Transform your career aspirations into achievements and embrace a future filled with passion and purpose with <strong><TextAnimation/></strong> by your side!</p>
+          <Button className="primary-btn" text="Download Our App" textColor="white" animation="slideIn 1.5s ease-out forwards; opacity: 0;" />
         </div>
-      </section>
-    );
+
+        <div className="hero-images">
+          {/* Example of using ImageComponent with different borderRadius */}
+          <ImageComponent 
+            src={images[(currentIndex + 1) % images.length]} 
+            alt="Student 1" 
+            className={`hero-image img-rounded ${fadeIn ? 'fade-in' : 'fade-out'}`} 
+            style={{ animationName: 'border-radius-animation-50' }} 
+          />
+          <ImageComponent 
+            src={images[(currentIndex + 2) % images.length]} 
+            alt="Student 2" 
+            className={`hero-image img-rounded ${fadeIn ? 'fade-in' : 'fade-out'}`} 
+            style={{ animationName: 'border-radius-animation-94' }} 
+          />
+          <ImageComponent 
+            src={images[(currentIndex + 3) % images.length]} 
+            alt="Student 3" 
+            className={`hero-image img-rounded ${fadeIn ? 'fade-in' : 'fade-out'}`} 
+            style={{ animationName: 'border-radius-animation-94' }} 
+          />
+          <ImageComponent 
+            src={images[(currentIndex + 4) % images.length]} 
+            alt="Student 4" 
+            className={`hero-image img-rounded ${fadeIn ? 'fade-in' : 'fade-out'}`} 
+            style={{ animationName: 'border-radius-animation-50' }} 
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
