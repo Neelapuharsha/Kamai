bb3e602f8c0959d9c47da715744fe4d6d837a5ed
MNB <m.nagabharath@gmail.com>
2024-06-20 21:33:00 +0530

    Bharath changes

--- src/App.js
+++ src/App.js
@@ -11,39 +11,57 @@ import Highlights from './components/Section/Highlights'
 import Sphere from './components/Sphere/Sphere';
 import Why from './components/problemWhy/why';
 import Header1 from './components/Header/Header1';
+import Whatk from './components/WhatSection/whatk';
 // import HighlightsMain from './components/Section/Highlights';
 
 
 function App() {
   return (
     <div className="App">
-    
+
       <Header1 />
-    
-      
       <Section
         borderTopRightRadius="0px"
         borderTopLeftRadius="0px"
       >
         <HeroSection />
       </Section>
-  
       <Section
+
+        backgroundImage="linear-gradient(45deg, #00c6ff, #0072ff)"
+        borderTopRightRadius="40px"
+        borderTopLeftRadius="40px"
+      >
+        <Why />
+
+      </Section>
+      <Section
+
         backgroundColor="#f1f6fb"
         borderTopRightRadius="40px"
         borderTopLeftRadius="40px"
       >
-        <Highlights/>
+        <Whatk />
 
       </Section>
-      
+
+      <Section
+      backgroundImage="linear-gradient(45deg, #00c6ff, #0072ff)"
+        // backgroundColor="#f1f6fb"
+        borderTopRightRadius="40px"
+        borderTopLeftRadius="40px"
+      >
+        <Highlights />
+
+      </Section>
+
       <Section
         backgroundColor="#fff"
         borderTopRightRadius="40px"
         borderTopLeftRadius="40px"
       >
 
-    <Contact />
+        <Contact />
       </Section>
 
 
@@ -55,19 +73,19 @@ function App() {
         borderTopLeftRadius="40px"
       >
         <Why />
-        
+
       </Section>
 
-      
+
       <Section
         backgroundColor="#111"
         borderTopRightRadius="40px"
         borderTopLeftRadius="40px"
       >
-      
+
         <Footer />
       </Section>
-      
+
     </div>
   );
 }
--- src/components/Section/Highlights.js
+++ src/components/Section/Highlights.js
@@ -6,31 +6,31 @@ const highlightsData = [
     image: 'https://img1.wsimg.com/isteam/stock/2akzpgl/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/fx-gs', // Random image from Unsplash
     title: 'Search Your Job in Your Native Language',
     keyHighlight: 'Job listings in multiple languages',
+    style: 'square', // Unique style
   },
   {
     image: 'https://www.essentialrecruitment.co.uk/wp-content/uploads/2023/03/Random-artwork-14.gif', // Random image from Unsplash
     title: 'Create Your Video Resume',
     keyHighlight: 'Record professional video resumes',
+    style: 'rect-vertical', // Unique style
   },
   {
     image: 'https://nestscale.com/wp-content/uploads/2022/08/mobile-friendly-1.jpg', // Random image from Unsplash
     title: 'Get Personalized Job Recommendations',
     keyHighlight: 'Tailored job suggestions',
-  },
-  {
-    image: 'https://thumbs.dreamstime.com/b/expert-advice-rubber-stamp-grunge-design-dust-scratches-effects-can-be-easily-removed-clean-crisp-look-color-easily-82605690.jpg', // Random image from Unsplash
-    title: 'Access Expert Career Advice',
-    keyHighlight: 'Tips on resume writing',
+    style: 'rect-horizontal', // Unique style
   },
   {
     image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjrHro6d3BTw7ZZ4IXgfb6_8aESB7-SsWfelDSSInZVamiMSnYpBZzGBaZBBrWxWwYgLqOHuOtroGvGjxrwzdUkhjwuIvM1u6chIblGKS1gQ6JVkjXr-Vztyk2zoYb1ylvhNgLgC5q6M-7LaiXT1xnAT96DvkPx89APNb8JEaz-1mnMRcfaOYYBHzQL/s1600/Text%20to%20Speech%20-%20Social%20-%201024x512.png', // Random image from Unsplash
     title: 'Text to Speech',
     keyHighlight: 'Convert job descriptions to audio',
+    style: 'square', // Unique style
   },
   {
     image: 'https://estricalla.hypotheses.org/files/2021/02/limitedexperience_hero_social.gif', // Random image from Unsplash
     title: 'Create Your Own Resume',
     keyHighlight: 'Build a resume with customizable templates',
+    style: 'rect-vertical', // Unique style
   },
 ];
 
@@ -38,13 +38,13 @@ const Highlights = () => {
   return (
     <div className="highlights-main">
       <div className="main-heading">
-        <h1>Our Standout Features</h1>
+        <h1>Key Features</h1>
       </div>
       <div className="highlights-article">
         {highlightsData.map((highlight, index) => (
-          <div className="highlight-card" key={index}>
+          <div className={`highlight-card ${highlight.style}`} key={index}>
             <div className="highlight-text-container">
-              <h3>{highlight.title}</h3>
+              <h4>{highlight.title}</h4>
             </div>
             <div className="highlight-image-container">
               <img src={highlight.image} alt={highlight.title} className="highlight-image" />
